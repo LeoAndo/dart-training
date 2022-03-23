186206f2a0f651e7f07f0e2a8a6ffac38114fcfb
LeoAndo <leo.777.info@gmail.com>
2022-03-23 19:33:19 +0900

    anonymous functions sample

---
+++ hello_dart/bin/anonymous_functions_sample01.dart
@@ -0,0 +1,14 @@
+void main(List<String> arguments) {
+  _test01();
+  print("==========================");
+}
+
+void _test01() {
+  bool Function(int) isEven = (int n) => n % 2 == 0;
+  print(isEven(2)); // true
+  print(isEven(3)); // false
+
+  var even = (int n) => n % 2 == 0;
+  print(even(2)); // true
+  print(even(3)); // false
+}
