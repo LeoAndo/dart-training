1dcdb2b1368764bd3111951e763321dddfade23c
LeoAndo <leo.777.info@gmail.com>
2022-03-28 16:41:47 +0900

    Factory constractorを使用してsingletonパターンで実装

--- singleton_pattern/bin/singleton_pattern.dart
+++ singleton_pattern/bin/singleton_pattern.dart
@@ -1,3 +1,13 @@
+import 'counter.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final counter = Counter();
+  // 100 loop.
+  for (var i = 0; i < 10; i++) {
+    counter.increment();
+  }
+  final counter2 = Counter();
+  print(counter.hashCode);
+  print(counter2.hashCode);
+  print(counter2.value);
 }
